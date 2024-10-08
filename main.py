36895e624eb0022f7969965f8f173e6f23997cf5
j-stax <ahnjames01@gmail.com>
2024-10-08 18:35:11 -0400

    Ship and asteroids

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     state = True
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+    
+    AsteroidField()
+    Player(x, y)
 
     while state:
         screen.fill((0, 0, 0))
+
+        for item in updatable:
+            item.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        # End loop when screen is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        delta = clock.tick(60)
+        dt = delta / 1000
 
 if __name__ == "__main__":
     main()
